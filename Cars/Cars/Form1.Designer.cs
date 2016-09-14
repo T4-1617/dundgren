b11aaf506ac0451edf817e31196cc8b861404dd6
Daniel Lundgren <danne.l.97@hotmail.com>
2016-09-14 15:46:26 +0200

    Changed label to textboxes and added buttons.

--- Cars/Cars/Form1.Designer.cs
+++ Cars/Cars/Form1.Designer.cs
@@ -30,7 +30,17 @@
         {
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -45,20 +55,117 @@
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.label1);
-            this.panel1.Location = new System.Drawing.Point(154, 13);
+            this.panel1.Controls.Add(this.textBox4);
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Location = new System.Drawing.Point(139, 13);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(118, 238);
+            this.panel1.Size = new System.Drawing.Size(133, 238);
             this.panel1.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(62, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(68, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(62, 30);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(68, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(62, 56);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(68, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(62, 82);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(68, 20);
+            this.textBox4.TabIndex = 3;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(4, 4);
+            this.label1.Location = new System.Drawing.Point(3, 7);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Fuel";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Horsep.";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 59);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Price";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 85);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Seats";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(21, 120);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Edit";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(21, 149);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Save";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(21, 178);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Cancel";
+            this.button3.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
@@ -79,7 +186,17 @@
 
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Cars/Cars/Form1.cs
+++ Cars/Cars/Form1.cs
@@ -22,18 +22,39 @@ namespace Cars
             panel1.Visible = false;
             //Skapar ett objekt(bil) och ger den värden till properties
             cars c = new cars() { name="Volvo", fuel="Gasoline", hp="170", price="300kr/h", seats="5"  };
+            cars d = new cars() { name = "BMW", fuel = "Magic", hp = "2000", price = "?kr/h", seats = "5" };
+            cars e = new cars() { name = "Mazda", fuel = "Diesel", hp = "150", price = "200kr/h", seats = "5" };
+            cars f = new cars() { name = "Mercedes-Benz", fuel = "Gasoline", hp = "200", price = "250kr/h", seats = "5" };
+            cars g = new cars() { name = "Audi", fuel = "Diesel", hp = "300", price = "500kr/h", seats = "7" };
             //Lägger till bilen i arraylisten
             MyCars.Add(c);
+            MyCars.Add(d);
+            MyCars.Add(e);
+            MyCars.Add(f);
+            MyCars.Add(g);
             //lägger till bilen i listan
             listBox1.Items.Add(c.name);
+            listBox1.Items.Add(d.name);
+            listBox1.Items.Add(e.name);
+            listBox1.Items.Add(f.name);
+            listBox1.Items.Add(g.name);
         }
 
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            cars c = (cars)MyCars[listBox1.SelectedIndex];
+            cars x = (cars)MyCars[listBox1.SelectedIndex];
 
             panel1.Visible = true;
-            label1.Text = string.Format(" Fuel: {0}\n Horsepowers: {1}\n Price: {2}\n Seats: {3}", c.fuel, c.hp, c.price, c.seats);
+            textBox1.Text = string.Format("{0}", x.fuel);
+            textBox2.Text = string.Format("{0}", x.hp);
+            textBox3.Text = string.Format("{0}", x.price);
+            textBox4.Text = string.Format("{0}", x.seats);
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
