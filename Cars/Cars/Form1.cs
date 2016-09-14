9765da8440ec0bffd3b98f4418b9dc246b459432
Daniel Lundgren <danne.l.97@hotmail.com>
2016-09-14 14:36:51 +0200

    Fixed listbox and made panel visible. Added ".name" to listbox.Items.Add. label1.text stopped working...

--- Cars/Cars/Form1.cs
+++ Cars/Cars/Form1.cs
@@ -21,11 +21,19 @@ namespace Cars
             //gömmer panelen
             panel1.Visible = false;
             //Skapar ett objekt(bil) och ger den värden till properties
-            Car c = new Car() { name="Volvo V70", fuel="Gasoline", hp="170", price="200.000kr", seats="5"  };
+            cars c = new cars() { name="Volvo V70", fuel="Gasoline", hp="170", price="300kr/h", seats="5"  };
             //Lägger till bilen i arraylisten
             MyCars.Add(c);
             //lägger till bilen i listan
-            listBox1.Items.Add(c);
+            listBox1.Items.Add(c.name);
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            cars c = (cars)listBox1.SelectedItem;
+
+            panel1.Visible = true;
+            label1.Text = string.Format(" Reg: {0}\n Fuel: {1}\n Horsepowers: {2}\n Price: {3}\n Seats: {4}", c.name, c.fuel, c.hp, c.price, c.seats);
         }
     }
 }
