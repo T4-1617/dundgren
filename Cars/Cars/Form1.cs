86ba240586b193bb973e505ff58b509971b61cfe
Daniel Lundgren <danne.l.97@hotmail.com>
2016-09-14 15:28:21 +0200

    Line 33 selecteditem to selectedindex

--- Cars/Cars/Form1.cs
+++ Cars/Cars/Form1.cs
@@ -21,7 +21,7 @@ namespace Cars
             //gömmer panelen
             panel1.Visible = false;
             //Skapar ett objekt(bil) och ger den värden till properties
-            cars c = new cars() { name="Volvo V70", fuel="Gasoline", hp="170", price="300kr/h", seats="5"  };
+            cars c = new cars() { name="Volvo", fuel="Gasoline", hp="170", price="300kr/h", seats="5"  };
             //Lägger till bilen i arraylisten
             MyCars.Add(c);
             //lägger till bilen i listan
@@ -30,10 +30,10 @@ namespace Cars
 
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            cars c = (cars)listBox1.SelectedItem;
+            cars c = (cars)MyCars[listBox1.SelectedIndex];
 
             panel1.Visible = true;
-            label1.Text = string.Format(" Reg: {0}\n Fuel: {1}\n Horsepowers: {2}\n Price: {3}\n Seats: {4}", c.name, c.fuel, c.hp, c.price, c.seats);
+            label1.Text = string.Format(" Fuel: {0}\n Horsepowers: {1}\n Price: {2}\n Seats: {3}", c.fuel, c.hp, c.price, c.seats);
         }
     }
 }
