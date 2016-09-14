b28dc0181c6f927f96ba93b8f981448fdc17c895
Daniel Lundgren <danne.l.97@hotmail.com>
2016-09-14 12:09:52 +0200

    List not arraylist.

---
+++ Customersdan/Customersdan/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace Customersdan
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtFirstname = new System.Windows.Forms.TextBox();
+            this.txtLastname = new System.Windows.Forms.TextBox();
+            this.lbMyCustomers = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Förnamn";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(123, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Efternamn";
+            // 
+            // txtFirstname
+            // 
+            this.txtFirstname.Location = new System.Drawing.Point(13, 30);
+            this.txtFirstname.Name = "txtFirstname";
+            this.txtFirstname.Size = new System.Drawing.Size(100, 20);
+            this.txtFirstname.TabIndex = 2;
+            // 
+            // txtLastname
+            // 
+            this.txtLastname.Location = new System.Drawing.Point(126, 29);
+            this.txtLastname.Name = "txtLastname";
+            this.txtLastname.Size = new System.Drawing.Size(100, 20);
+            this.txtLastname.TabIndex = 3;
+            // 
+            // lbMyCustomers
+            // 
+            this.lbMyCustomers.FormattingEnabled = true;
+            this.lbMyCustomers.Location = new System.Drawing.Point(12, 131);
+            this.lbMyCustomers.Name = "lbMyCustomers";
+            this.lbMyCustomers.Size = new System.Drawing.Size(259, 95);
+            this.lbMyCustomers.TabIndex = 4;
+            this.lbMyCustomers.SelectedIndexChanged += new System.EventHandler(this.lbMyCustomers_SelectedIndexChanged_1);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 75);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "+";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 239);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 6;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(126, 80);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(56, 17);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "Active";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lbMyCustomers);
+            this.Controls.Add(this.txtLastname);
+            this.Controls.Add(this.txtFirstname);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtFirstname;
+        private System.Windows.Forms.TextBox txtLastname;
+        private System.Windows.Forms.ListBox lbMyCustomers;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Customersdan/Customersdan/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Customersdan
+{
+    public partial class Form1 : Form
+    {
+        string status;
+        //int för att hålla reda på id
+        int i = 100;
+        //En lista som ska innehålla kundobjekten
+        List<Customer> MyCustomers = new List<Customer>();
+
+        public Form1()
+        {
+
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //skapar ett objekt med hjälp av klassen 
+            Customer c = new Customer();
+            c.id = i;
+            c.firstname = txtFirstname.Text;
+            c.lastname = txtLastname.Text;
+
+            if (checkBox1.Checked)
+            {
+                c.active = true;
+            }
+
+            else {
+                c.active = false;
+            }
+            //lägger till kunden i listan
+            MyCustomers.Add(c);
+            //tömmer listboxen innan nya kunder läggs till
+            lbMyCustomers.Items.Clear();
+            //går igenom listan och lägger till varje kund
+            foreach (Customer item in MyCustomers) {
+
+                lbMyCustomers.Items.Add(item.fullname());
+
+            }
+
+
+            //ändrar labeln så att de berättar hur många kunder det finns i listan
+            label3.Text = string.Format("Du har {0} kunder", MyCustomers.Count);
+
+            //lägger till 1 på int i så att nästa kund får nytt id
+            i++;
+
+           
+
+
+
+        }
+
+
+        private void lbMyCustomers_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            //kollar var i listan man klickar
+            Customer c = MyCustomers[lbMyCustomers.SelectedIndex];
+
+            MessageBox.Show(string.Format("ID: {0}\nFirst name: {1}\nLast name {2}\nStatus: {3}", 
+                c.id, c.firstname, c.lastname, c.active));
+        }
+    }
+}
