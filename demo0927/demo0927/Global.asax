678878bc36597b74833a9a4df5047061536678da
Daniel Lundgren <danne.l.97@hotmail.com>
2016-09-27 15:37:42 +0200

    Init commit. Created links in a list. Linked css-file.

---
+++ demo0927/demo0927/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="demo0927.MvcApplication" Language="C#" %>
---
+++ demo0927/demo0927/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(demo0927.Startup))]
+namespace demo0927
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
